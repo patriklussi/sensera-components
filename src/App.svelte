3308a7c9250ea44fb2b3c5c3729aee4bcb2a6fb5
patriklussi <patrik.lussi@gmail.com>
2022-04-09 01:44:20 +0200

    added

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,11 @@
 <script>
-	export let name;
+	
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<button class="buttonBlue"></button>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.buttonBlue {
+		background: blue;
 	}
 </style>
