9cb656600e6317845ae708efc9c3ffb02f045ba4
patriklussi <patrik.lussi@gmail.com>
2022-04-09 01:10:36 +0200

    ada

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/*
-export default function helloThere(){
-    console.log("Hello there");
-
-}
-*/
-
 export default function renderThing(){
    let tempName = document.createElement("p");
    tempName.innerHTML="Wax on wax off"
