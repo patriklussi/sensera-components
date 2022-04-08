2f06c1b8f8169f566ceb8e21a24282e118df143d
patriklussi <patrik.lussi@gmail.com>
2022-04-09 01:05:07 +0200

    aa

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ export default function helloThere(){
     console.log("Hello there");
 
 }
+
+export default function renderThing(){
+   let tempName = document.createElement("p");
+   tempName.innerHTML="Wax on wax off"
+    document.appendChild(tempName);
+}
+
