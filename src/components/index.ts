6f518e364b8593aae8882ac373ffcc80ca7e86f4
patriklussi <patrik.lussi@gmail.com>
2022-04-10 01:13:30 +0200

    Installed mui and added shadows

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
-export {default as Button} from "./Button"
-export {default as Rectangle} from "./Rectangle"
 export {default as Darktheme} from "./Darkthemes"
 export {default as Lightthemes} from "./Lightthemes"
+export {default as Shadows} from "./Shadows"
