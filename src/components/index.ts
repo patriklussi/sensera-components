cb713b004c9a857dd6ee113cc2d1635f2f22c836
patriklussi <patrik.lussi@gmail.com>
2022-04-10 00:52:30 +0200

    Big commito

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export {default as Button} from "./Button"
 export {default as Rectangle} from "./Rectangle"
-export {default as Darktheme} from "./Themes"
+export {default as Darktheme} from "./Darkthemes"
+export {default as Lightthemes} from "./Lightthemes"
