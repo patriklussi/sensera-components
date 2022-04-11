1694c01b105f6fa94874ee499ec6ab18cb9a7b62
patriklussi <patrik.lussi@gmail.com>
2022-04-11 16:21:41 +0200

    Added breakpoints and container

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export {default as darkThemeColors} from "./darkThemeColors"
 export {default as lightThemeColors} from "./lightThemeColors"
 export {default as Globals} from "./Globals"
+export {default as breakpoints} from "./breakpoints"
+export {default as container} from "./container"
